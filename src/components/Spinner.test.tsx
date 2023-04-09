2c9f1dbe51d5d6949e20ff7da2d9560badfc37a9
Meeyad Shabab <mmshabab90@gmail.com>
2023-04-09 15:16:12 +0200

    added eslint parser, added spinner react component test

---
+++ src/components/Spinner.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react"
+import Spinner from "./Spinner"
+import '@testing-library/jest-dom/extend-expect';
+
+test("Render the spinner with the provided child prop", () => {
+    const renderSpinner = render(<Spinner>Loading content...</Spinner>)
+    const spinnerElement = renderSpinner.container.querySelector("#spinner")
+    const childElement = screen.getByRole("spinner-content")
+    expect(childElement).toHaveTextContent("Loading content...")
+    expect(spinnerElement).toHaveAttribute("role", "status")
+})
--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -10,12 +10,13 @@ const Spinner: FC<Props> = ({ children }) => {
             <div
                 className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
                 role="status"
+                id="spinner"
             >
                 <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
                     Loading...
                 </span>
             </div>
-            <p>{children}</p>
+            <p role="spinner-content">{children}</p>
         </>
     )
 }
